15200f9307e4cf051bfff593fbf65a2078e53686
Akshith <gakshithrao1663@gmail.com>
2022-10-30 11:26:18 +0530

    Add pre-commit hook to run tests

---
+++ todo.js
@@ -0,0 +1,80 @@
+function todoList() {
+    all = [];
+    const add = (todoItem) => {
+      all.push(todoItem);
+    };
+    const markAsComplete = (index) => {
+      all[index].completed = true;
+    };
+  
+    const overdue = () => {
+      // Write the date check condition here and return the array of overdue items accordingly.
+      // FILL YOUR CODE HERE
+      // ..
+      // ..
+      // ..
+      return all.filter((item) => {
+        return item.dueDate <= yesterday && item.completed === false;
+      });
+    };
+  
+    const dueToday = () => {
+      // Write the date check condition here and return the array of todo items that are due today accordingly.
+      // FILL YOUR CODE HERE
+      // ..
+      // ..
+      // ..
+      return all.filter((item) => {
+        return item.dueDate === today;
+      });
+    };
+  
+    const dueLater = () => {
+      // Write the date check condition here and return the array of todo items that are due later accordingly.
+      // FILL YOUR CODE HERE
+      // ..
+      // ..
+      // ..
+      return all.filter((item) => {
+        return item.dueDate === tomorrow && item.completed === false;
+      });
+    };
+  
+    const toDisplayableList = (list) => {
+      // Format the To-Do list here, and return the output string as per the format given above.
+      // FILL YOUR CODE HERE
+      // ..
+      // ..
+      // ..
+      // return OUTPUT_STRING
+      let mylist = [];
+      list.forEach((item) => {
+        if (item.dueDate === today) {
+          if (item.completed === true) {
+            mylist.push(`[x] ${item.title}`);
+          } else {
+            mylist.push(`[ ] ${item.title}`);
+          }
+        } else {
+          if (item.completed === true) {
+            mylist.push(`[x] ${item.title} ${item.dueDate}`);
+          } else {
+            mylist.push(`[ ] ${item.title} ${item.dueDate}`);
+          }
+        }
+      });
+      return mylist.join("\n");
+    };
+  
+    return {
+      all,
+      add,
+      markAsComplete,
+      overdue,
+      dueToday,
+      dueLater,
+      toDisplayableList,
+    };
+  }
+    
+    module.exports=todoList;
